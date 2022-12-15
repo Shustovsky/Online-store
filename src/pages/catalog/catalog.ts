91555541dbd246c0b4678828d1b93bc1c95c9c4b
A. Shustovsky <a.shustovsky@gmail.com>
2022-12-15 20:33:02 +0400

    feat: add function createFilter

--- src/pages/catalog/catalog.ts
+++ src/pages/catalog/catalog.ts
@@ -1,4 +1,6 @@
-export default function createCatalog(): void {
+import { Product } from '../../model/product';
+
+export default function createCatalog(products: Product[]): void {
     const header = document.querySelector('.header') as HTMLElement;
 
     const main = document.createElement('main');
@@ -10,7 +12,7 @@ export default function createCatalog(): void {
 
     const mainContainer = document.createElement('div');
     mainContainer.className = 'main_container';
-    container.append(mainContainer)
+    container.append(mainContainer);
 
     const filters = document.createElement('div');
     filters.className = 'filters';
@@ -32,4 +34,50 @@ export default function createCatalog(): void {
     btnCopy.innerHTML = 'Reset Filters';
     filtersButtons.append(btnCopy);
 
+    function createFilter(name: string): void {
+        const filtersCategory = document.createElement('div');
+        filtersCategory.className = `filters__${name}`;
+        filters.append(filtersCategory);
+
+        const filtersCheckbox = document.createElement('fieldset');
+        filtersCheckbox.className = 'filters-checkbox';
+        filtersCategory.append(filtersCheckbox);
+
+        const legend = document.createElement('legend');
+        legend.innerHTML = `${name}`;
+        filtersCheckbox.append(legend);
+
+        const allCategory: string[] = [];
+
+        for (const item of products) {
+            //захаркодил т.к. let categoryItem = item[name]; по каким-то причинам не работает
+            let categoryItem = '';
+            if (name === 'category') {
+                categoryItem = item.category;
+            } else if (name === 'brand') {
+                categoryItem = item.brand;
+            }
+            if (!allCategory.includes(categoryItem)) {
+                allCategory.push(categoryItem);
+            }
+        }
+        allCategory.forEach((item) => {
+            const div = document.createElement('div');
+            filtersCheckbox.append(div);
+
+            const input = document.createElement('input');
+            input.type = 'checkbox';
+            input.id = `${item}`;
+            input.name = `${item}`;
+            div.append(input);
+
+            const label = document.createElement('label');
+            label.setAttribute('for', `${item}`);
+            label.innerHTML = `${item}`;
+            div.append(label);
+        });
+    }
+
+    createFilter('category');
+    createFilter('brand');
 }
