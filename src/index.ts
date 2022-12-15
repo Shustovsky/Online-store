8c119edaa3cec3e743670ed1b66d13a17ac3bb45
A. Shustovsky <a.shustovsky@gmail.com>
2022-12-15 20:32:34 +0400

    refactor: add import products.json

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import './main.scss';
 import createHeader from './pages/components/header';
-import createCatalog from "./pages/catalog/catalog";
+import createCatalog from './pages/catalog/catalog';
+import products from './assets/json/products.json';
 
 createHeader({ totalPrice: '€2,428.00', productsCount: 3 }); // Установлены значения для примера
-createCatalog();
+createCatalog(products.products);
