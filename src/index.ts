19e76c15460efc0332c03466dec02a688c3cfb70
NadzeyaShu <chunta89@gmail.com>
2022-12-20 18:04:58 +0400

    feat: shopping cart view impl

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
 import './main.scss';
-import createHeader from './pages/components/header';
-import createCatalog from './pages/catalog/catalog';
-import products from './assets/json/products.json';
-
-createHeader({ totalPrice: 2428.01, productsCount: 3 }); // Установлены значения для примера
-createCatalog(products.products);
--- src/model/shoppingCart.ts
+++ src/model/shoppingCart.ts
@@ -1,4 +1,7 @@
+import { Product } from './product';
+
 export type ShoppingCart = {
     totalPrice: number;
     productsCount: number;
+    products: { product: Product; count: number }[];
 };
