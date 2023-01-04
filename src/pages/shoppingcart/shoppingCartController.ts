018ccfff5c4ea543c5a70a426b181f90a5af0016
NadzeyaShu <chunta89@gmail.com>
2023-01-04 18:38:50 +0400

    feat: add modal validation, btn submit validation and add/delete on header

--- src/pages/shoppingcart/shoppingCartController.ts
+++ src/pages/shoppingcart/shoppingCartController.ts
@@ -1,14 +1,22 @@
 import { ShoppingCartView } from './shoppingCartView';
 import { ShoppingcartService } from './shoppingcartService';
 import { HeaderView } from '../components/headerView';
+import { ShoppingcartValidator } from './shoppingcartValidationServise';
 
 export class ShoppingCartController {
     shoppingcartService: ShoppingcartService;
+    shoppingcartValidator: ShoppingcartValidator;
     shoppingCartView: ShoppingCartView;
     headerView: HeaderView;
 
-    constructor(shoppingcartService: ShoppingcartService, shoppingCartView: ShoppingCartView, headerView: HeaderView) {
+    constructor(
+        shoppingcartService: ShoppingcartService,
+        shoppingcartValidator: ShoppingcartValidator,
+        shoppingCartView: ShoppingCartView,
+        headerView: HeaderView
+    ) {
         this.shoppingcartService = shoppingcartService;
+        this.shoppingcartValidator = shoppingcartValidator;
         this.shoppingCartView = shoppingCartView;
         this.headerView = headerView;
     }
@@ -28,5 +36,80 @@ export class ShoppingCartController {
         const shoppingCart = this.shoppingcartService.deleteItem(productId);
         this.shoppingCartView.onShoppingCardChange(shoppingCart);
         this.headerView.onShoppingCardChange(shoppingCart);
+    }
+
+    clearShoppingCart() {
+        const shoppingCart = this.shoppingcartService.clearShoppingCart();
+        this.headerView.onShoppingCardChange(shoppingCart);
+    }
+
+    validateName(name: string): boolean {
+        if (this.shoppingcartValidator.validateName(name)) {
+            this.shoppingCartView.deleteNameError();
+            return true;
+        } else {
+            this.shoppingCartView.createNameError();
+            return false;
+        }
+    }
+
+    validatePhone(phoneNumber: string): boolean {
+        if (this.shoppingcartValidator.validatePhoneNumber(phoneNumber)) {
+            this.shoppingCartView.deletePhoneNumberError();
+            return true;
+        } else {
+            this.shoppingCartView.createPhoneNumberError();
+            return false;
+        }
+    }
+
+    validateAddress(address: string): boolean {
+        if (this.shoppingcartValidator.validateAddress(address)) {
+            this.shoppingCartView.deleteAddressError();
+            return true;
+        } else {
+            this.shoppingCartView.createAddressError();
+            return false;
+        }
+    }
+
+    validateEmail(email: string): boolean {
+        if (this.shoppingcartValidator.validateEmail(email)) {
+            this.shoppingCartView.deleteEmailError();
+            return true;
+        } else {
+            this.shoppingCartView.createEmailError();
+            return false;
+        }
+    }
+
+    validateCartNumber(cartNumber: string): boolean {
+        if (this.shoppingcartValidator.validateCartNumber(cartNumber)) {
+            this.shoppingCartView.deleteModalCartNumberError();
+            return true;
+        } else {
+            this.shoppingCartView.createModalCartNumberError();
+            return false;
+        }
+    }
+
+    validateThru(thru: string): boolean {
+        if (this.shoppingcartValidator.validateThru(thru)) {
+            this.shoppingCartView.deleteThruError();
+            return true;
+        } else {
+            this.shoppingCartView.createThruError();
+            return false;
+        }
+    }
+
+    validateCvv(cvv: string): boolean {
+        if (this.shoppingcartValidator.validateCVV(cvv)) {
+            this.shoppingCartView.deleteCvvError();
+            return true;
+        } else {
+            this.shoppingCartView.createCvvError();
+            return false;
+        }
     }
 }
